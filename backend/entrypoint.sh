f68b7b89d7f7aec8ecf66710afa1a352483a8ee8
Curtis Wirtz <supcurtis@gmail.com>
2025-12-03 23:51:34 -0800

    entrypoint should not run collectstatic in development

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,17 +6,23 @@ set -e
 DB_HOST=$DATABASE_HOST
 DB_PORT=$DATABASE_PORT
 
-echo "Entrypoint script: Waiting for PostgreSQL at $DB_HOST:$DB_PORT..."
+echo "Entrypoint: Waiting for PostgreSQL at $DB_HOST:$DB_PORT..."
 
 # This loop checks if the database is ready by trying to connect to it.
 until printf "" 2>>/dev/null >>/dev/tcp/$DB_HOST/$DB_PORT; do
-  echo "PostgreSQL is unavailable - sleeping for 1 second"
+  echo "Entrypoint: PostgreSQL is unavailable - sleeping for 1 second"
   sleep 1
 done
 
-echo "Entrypoint script: PostgreSQL started - executing command"
+echo "Entrypoint: PostgreSQL has started"
 
+echo "Running Django migrations..."
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+
+# Optionally collect static ONLY in production
+if [ "$DJANGO_ENV" = "prod" ]; then
+    echo "Collecting static files..."
+    python manage.py collectstatic --noinput
+fi
 
 exec "$@"

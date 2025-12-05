aad44f3d00fb993deeba3cb5c605ed3b665bbad1
Curtis Wirtz <supcurtis@gmail.com>
2025-12-05 01:53:45 -0800

    update production settings.py with cors and csrf .env variables

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -3,9 +3,9 @@ import os
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
-DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower() in ("1", "true", "yes") # DBUG is always False in production
+DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower() in ("1", "true", "yes") # DEBUG is always False in production
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
@@ -23,7 +23,12 @@ DATABASES = {
     }
 }
 
-CSRF_TRUSTED_ORIGINS = os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS', 'http://localhost:8000').split(',')
+# Allows request that include HttpOnly cookies, Authorization: Bearer, credentials "include" in fetch
+CORS_ALLOW_CREDENTIALS = True
+# Allows Django to accept cookies + CSRF tokens
+CSRF_TRUSTED_ORIGINS = os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS').split(',')
+# Allows JavaScript in the browser make requests to the Django API from another domain/port
+CORS_ALLOWED_ORIGINS = os.environ.get('DJANGO_CORS_ALLOWED_ORIGINS').split(',')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
